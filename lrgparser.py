e4f27578d38685ec3615a3e2f116c27f0f5e6ce1
Rebecca Haines <haines.rebecca.l@gmail.com>
2014-11-14 05:14:22 -0800

    updated code with tests included

--- lrgparser.py
+++ lrgparser.py
@@ -6,9 +6,16 @@ def lrg_parse(filename):
 	this function opens the xml file and gets the tree and root
 	'''
 	# opens the file and parses the XML
-	tree = ET.parse(filename)
-	root = tree.getroot()
-        return tree
+	
+	try: #test to see if the file is an xml, the file is an lrg and the version of the lrg is 1.8
+		tree = ET.parse(filename)
+		root = tree.getroot()
+		assert root.tag == 'lrg', "Not an lrg file"
+		assert root.attrib == {'schema_version': '1.8'}, "Incorrect schema version. This tool only supports version 1.8"
+	except IOError:
+		print "Error: Not an xml file or ElementTree not installed"
+	return tree
+
 
 ###################################
 # Iterate through tree and look for exon tags, selecting only those with "label"
@@ -18,9 +25,9 @@ def lrg_parse(filename):
 ###################################
 
 def lrg_sequence(tree):
-        '''
-        to get the genomic sequence
-        '''
+	'''
+	to get the genomic sequence
+	'''
 	# results list
 	seq_max_len = 0
 	gsequence = ""
@@ -31,21 +38,46 @@ def lrg_sequence(tree):
 			if len(element.text) > seq_max_len:
 				seq_max_len = len(element.text)
 				gsequence = element.text
-	return gsequence	
+					
+	#Checks that gsequence contains only A, C, T, G.
+	nucleotides = ['A','C','T','G']
+
+	for nuc in gsequence:
+    		assert nuc in nucleotides, "Mistake- sequence contains characters other than A, T, C or G"
+
+	return gsequence
+
 
 def lrg_exoncoord(tree):
-        '''
-        to get the exon coordinates
-        '''
+	'''
+	to get the exon coordinates
+	'''
 	exons = []
 
-        for element in tree.iter():
-        # get the start and finish coordinates for the exons
+	for element in tree.iter():
+	# get the start and finish coordinates for the exons
 		if element.tag == "exon" and "label" in element.attrib:
 			for subelement in element:
 				if "t" not in subelement.attrib["coord_system"] and "p" not in subelement.attrib["coord_system"]:
-					coords = int(subelement.attrib["start"]), int(subelement.attrib["end"])
-					exons.append(coords)
+					#Checking input coordinates are numerical characters that can be converted to integers					
+					try:					
+						coords = int(subelement.attrib["start"]), int(subelement.attrib["end"])
+					except TypeError:
+						print "input coordinates are not integers"	
+				
+					exons.append(coords)	
+	
+	assert exons != [], "List of exon co-ordinates is empty. No exon co-ordinates stored."
+	for tup in exons:
+		try:
+			assert len(tup) == 2, "Error: Missing co-ordinate in list of exon co-ordinates"
+			assert tup[0] < tup[1], "Second co-ordinate of co-ordinate pair bigger than first"
+		except TypeError:
+			print "Error: Missing co-ordinate in list of exon co-ordinates"	
+
+	for tupInd in range(len(exons)-1):
+		assert exons[tupInd][1] < exons[tupInd+1][0], "Exon n starts before the end of exon n-1"
+
 	return exons
 
 def sequence_slicer(sequence, coords):
@@ -54,20 +86,19 @@ def sequence_slicer(sequence, coords):
 	These are used to output a FASTA formatted file that contains the sections of 
 	sequence defined by the coordinates.
 	'''
-	exons = []
 	for exon in range(0, len(coords)):
-		
+	
 		start, end = coords[exon]
 		start = int(start)
 		end = int(end)
-		info = "exon %d start: %d, end: %d" % (exon+1, start, end)
+		print ">exon %d start: %d, end: %d" % (exon+1, start, end)
+		#exon name could be taken directly from file to account for eg exon 1b.
 		# start must be -1 for indexing, end is ok as the slice locations are between positions
-		exon = info, sequence[start-1: end]
-		exons.append(exon)
-	return exons
+		print sequence[start-1: end]
 
 tree = lrg_parse(argv[1])
 gsequence = lrg_sequence(tree)
 exons = lrg_exoncoord(tree)
 sequence_slicer(gsequence, exons)
 
+#lrg_parser(filename, upstream = 0, downstream = 0)
