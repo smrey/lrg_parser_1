2e8cad56c913cdde2f99991898474541deb3bf3c
Sara <sara.rey@postgrad.manchester.ac.uk>
2014-11-14 05:42:09 -0800

    Added an additional test (last coordinate within length of sequence) and changed minimum exon length to 1

--- testingmaterials/test_7.py
+++ testingmaterials/test_7.py
@@ -1,8 +1,9 @@
 #Stop if the exon has length <2 bases?
+#Added test to make sure that the 
 ##ADD INTO THE FOLLOWING FUNCTION THE LINES WHICH I HAVE COMMENTED AFTER
 
 sequence =  'ACGTACGTACGTACGTACGTACGTACGTACGT' #####We don't need this stuff, I just wanted it to operate independently here######
-coords =  [(1,2), (6,12)]#####We don't need this stuff, I just wanted it to operate independently here#####
+coords =  [(1,2), (6,12), (16,33)]#####We don't need this stuff, I just wanted it to operate independently here#####
 
 
 def sequence_slicer(sequence, coords):
@@ -16,12 +17,13 @@ def sequence_slicer(sequence, coords):
 		start, end = coords[exon]
 		start = int(start)
 		end = int(end)
+		assert end <= len(sequence), "Co-ordinates lie outside of the range of the sequence"
 		info = "exon %d start: %d, end: %d" % (exon+1, start, end)
 		# start must be -1 for indexing, end is ok as the slice locations are between positions
 		exon = info, sequence[start-1: end]
 		exonSeq = sequence[start-1: end] #####Here is appended line######
-		print exonSeq
-		assert len(exonSeq) >= 3, "Exon is not long enough, it has fewer than 3 nucleotides" ######Here is appended line######
+		#print exonSeq
+		assert len(exonSeq) > 0, "Exon is not long enough, it has fewer than 3 nucleotides" ######Here is appended line######
 		exons.append(exon)
 	return exons
 
