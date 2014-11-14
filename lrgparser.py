e27950229e49586c10276f757888b176dd2b7a96
Laura Lopez Pascua <laura.lopezpascua@postgrad.manchester.ac.uk>
2014-11-14 03:42:43 -0800

    outputs to FASTA happily. File name is the LRG ID number (+.fa)

--- lrgparser.py
+++ lrgparser.py
@@ -60,15 +60,57 @@ def sequence_slicer(sequence, coords):
 		start, end = coords[exon]
 		start = int(start)
 		end = int(end)
-		info = "exon %d start: %d, end: %d" % (exon+1, start, end)
+		info = "ex%d|start: %d|end: %d" % (exon+1, start, end)
 		# start must be -1 for indexing, end is ok as the slice locations are between positions
 		exon = info, sequence[start-1: end]
 		exons.append(exon)
 	return exons
 
-def fasta_output(exons, outfile="lrgparser_output"):
+#######################################################################################################
+#
+#	THESE ARE THE BITS I HAVE ADDED
+#	COPY INTO MASTER AS MERGING IS HORRIBLE
+#	
+#	 |||||||
+#	 to end!
+#	 |||||||
+#	 |||||||
+#	\	/
+#	 \     /
+#	  \   /
+#          \ /
+#           V
+#
+#######################################################################################################
+
+def get_gene_info(tree):
 	'''
-	Writes the list of exons to a fasta file
+	Looks through the xml tree to find the details of the gene
+	Returns a tuple ordered as (Accession number, LRG ID number, gene name)
+	'''
+	# default values in case nothing is found, so the rest of the program will work
+	accnumber = "Accession_Number_Not_Found"
+	LRGid = "LRG_ID_Not_Found"
+	gname = "Gene_Name_Not_Found"
+
+	# get the key details from the xml
+	# NOTE: if multiples are found, this will keep the last one
+	for node in tree.iter('sequence_source'):
+		accnumber = node.text
+	for node in tree.iter('id'):
+		LRGid = node.text
+	for node in tree.iter('lrg_locus'):
+		gname = node.text
+
+	assert accnumber != "", "Accession number has no assigned value"
+	assert LRGid != "", "LRG ID has no assigned value"
+	assert gname != "", "Gene name has no assigned value"
+
+	return accnumber, LRGid, gname
+
+def fasta_output(exons, accession, outfile):
+	'''
+	Writes the list of exons to a fasta file.
 	'''
 	try: 
 		out = open(outfile, "w")
@@ -77,25 +119,46 @@ def fasta_output(exons, outfile="lrgparser_output"):
 
 	for exon in exons:
 		header, sequence = exon
+		###########################################################
+		# This bit adds the accession number to the start of the 
+		# FASTA header. I would prefer to do this in sequence_slicer,
+		# but don't want to cock up the github
+		#
+		out.write(">")
+		out.write(accession)
+		###########################################################
 		out.write(header)
+		out.write("\n")
+
 		# write the sequence with a newline every 80 characters,
 		# for readability and also (i think) to meet FASTA standard
 		# there is probably no reason for this not to be hard coded
-		for i in range(0, len(sequence)):
+		#
+		# CHECK THE FULL LENGTH EXON IS PRINTING
+		#
+
+		for i in range(1, len(sequence)):
 			if i % 80 == 0:
 				out.write(sequence[i])
 				out.write("\n")
 			else:
 				out.write(sequence[i])
-		# add an empty line between exons
+		# add an empty line between exons, for neatness
 		out.write("\n\n")
 
+def run_parser(infile):
+	'''
+	To run, combines all the functions into a (hopefully) sensible thing
+	'''
+	tree = lrg_parse(infile)
+	gsequence = lrg_sequence(tree) 
+	exons = lrg_exoncoord(tree)
+	exon_sequences = sequence_slicer(gsequence, exons)
+	accession, lrgid, gname = get_gene_info(tree)
+	out = lrgid + ".fa"
+	fasta_output(exon_sequences, accession, out)
+
 # Ideally the argv options should be properly parsed, this may balls up with the wrong number of inputs...
-tree = lrg_parse(argv[1])
-gsequence = lrg_sequence(tree)
-exons = lrg_exoncoord(tree)
-exon_sequences = sequence_slicer(gsequence, exons)
-if len(argv) > 2:
-	fasta_output(exon_sequences, argv[2])
-else:
-	fasta_output(exon_sequences)
+infile = argv[1]
+run_parser(infile)
+
